6f6d65954f71bb28f0965c36e73f0b50f9535c0e
cory2dragon <55415792+cory2dragon@users.noreply.github.com>
2020-03-18 14:15:27 -0700

    Add function from gary's code

--- script.js
+++ script.js
@@ -7,8 +7,28 @@ function countUp(evt){
     console.log(counter);
     $(divN).text(counter);
   }
+}//end countUp
+function onChange(evt) {
+  let correct = $(this).data("correct");
+  let response = $(this).val();
+  console.log(md5(response));
+  if (correct == md5(response)) {
+    $(this)
+      .removeClass("incorrect")
+      .addClass("correct");
+    let theScore = Number($("#score").text());
+    theScore = theScore + 1;
+    $("#score").text(theScore);
+  } else {
+    if (Math.random() > 0.1) {
+      sad.play();
+    }
+    $(this)
+      .removeClass("correct")
+      .addClass("incorrect");
+  }
+}//end onChange
 
-}
-
+$("input").change(onChange);
 countUp();
 setInterval(countUp,1000);
